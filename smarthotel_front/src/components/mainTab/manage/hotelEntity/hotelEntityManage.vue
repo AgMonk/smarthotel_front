de5f84c0c244900ab1201a26cb0007c232dc21d1
ginstone <hjg719@139.com>
2021-03-04 16:36:28 +0800

    显示表格

--- smarthotel_front/src/components/mainTab/manage/hotelEntity/hotelEntityManage.vue
+++ smarthotel_front/src/components/mainTab/manage/hotelEntity/hotelEntityManage.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    {{ prefix }}
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="20">
+            <el-form :inline="true">
+              <el-form-item label="类型">
+                <el-input v-model="param.pageData.type"/>
+              </el-form-item>
+              <el-form-item label="关键字">
+                <el-input v-model="param.pageData.keyword"/>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @close="findPageData">查询</el-button>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary">添加</el-button>
+              </el-form-item>
+            </el-form>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-table :data="data">
+          <el-table-column prop="id" label="id"/>
+          <el-table-column prop="number" label="编号"/>
+          <el-table-column prop="name" label="名称"/>
+          <el-table-column v-if="parentPrefix!==undefined" prop="parent.name" label="上级ID"/>
+        </el-table>
+      </el-main>
+      <el-footer>foot</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -14,6 +44,7 @@ let entityTemplate = {
   number: 0,
   name: "",
   parentId: 0,
+  parent: undefined,
 }
 
 export default {
@@ -45,16 +76,30 @@ export default {
     del() {
       this.cudRequest("del")
     },
+    getParent() {
+      if (this.parentPrefix === undefined || this.parentData === undefined || this.data === undefined) {
+        return;
+      }
+      let t = this.data;
+      t.forEach(item => {
+        console.log(item)
+        item.parent = this.parentData.filter(i => i.id === item.id)[0];
+        console.log(item)
+      })
+      this.data = t;
+      console.log(this.data)
+    },
     findParentAll() {
       if (this.parentPrefix === undefined) {
         return;
       }
       getData(false, this.parentPrefix, 10, req,
-      {
-        url: "/hotelEntity/{prefix}/findAll".format({prefix: this.parentPrefix}),
-        success: this.success
-      }).then(res => {
+        {
+          url: "/hotelEntity/{prefix}/findAll".format({prefix: this.parentPrefix}),
+          success: this.success
+        }).then(res => {
         this.parentData = res.data;
+        this.getParent();
       })
     },
     findPageData() {
@@ -62,10 +107,11 @@ export default {
         {
           url: this.getUrl("findPageData"),
           success: this.success,
-          data:this.param.pageData,
+          data: this.param.pageData,
         }
-      ).then(res=>{
-        this.data = res.data;
+      ).then(res => {
+        this.data = res.data.data;
+        this.getParent();
       })
     },
     //写操作请求
